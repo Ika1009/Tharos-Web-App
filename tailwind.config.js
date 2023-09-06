2010f2dc968c87f6c91bc2e9c5b58c1e72da33d2
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-06 23:27:18 +0200

    header

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+      extend: {
+        colors: {
+          'custom-black': '#242121',
+          'custom-purple': '#5065A8',
+          'custom-dark-blue': '#0e0d2f',
+          'custom-light-blue': '#0bc1f5'
+        },
+      },
+    },
+    plugins: [],
+  
+  }
