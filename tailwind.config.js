fbd17b52475ee6b9ab0912d7a1276be739780522
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-12 10:38:44 +0200

    register page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["index.html"],
+    content: ["report.html", "register.html"],
     theme: {
       extend: {
         colors: {
