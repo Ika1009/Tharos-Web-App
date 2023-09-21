0dee808e6e7d1042735290ece80b730253621d7d
Ika1009 <ilija.doncic@yahoo.com>
2023-09-21 18:45:06 +0200

    new dbconn

--- DB_APIs/db_conn.php
+++ DB_APIs/db_conn.php
@@ -1,23 +1,36 @@
 
 <?php
 
-$host = '162.0.232.232';
-$db   = 'tharcgyg_tharros';
-$user = 'tharcgyg_webapp';
-$pass = 'webapptharos';
+// $host = 'localhost';
+// $db   = 'tharcgyg_tharros';
+// $user = 'tharcgyg_webapp';
+// $pass = 'webapptharos';
+// $charset = 'utf8mb4';
+
+// $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+// $options = [
+//     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//     PDO::ATTR_EMULATE_PREPARES   => false,
+// ];
+
+// try {
+//     $pdo = new PDO($dsn, $user, $pass, $options);
+// } catch (\PDOException $e) {
+//     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+// }
+
+$servername = 'localhost';
+$username = 'bonanza_nemanja';
+$password = 'Bonanza123';
+$dbname = "bonanza_maildelivery";
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+if($conn->connect_error)
+{
+    die("connection failed: " . $conn->connect_error);
 }
 
 ?>
