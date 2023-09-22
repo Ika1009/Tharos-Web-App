878718f8855f195835b90ed23016aa41bc153047
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-22 19:30:36 +0200

    logout path

--- report/report.php
+++ report/report.php
@@ -39,7 +39,7 @@
                   class="uppercase py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-custom-light-blue md:p-0 md:dark:hover:text-custom-light-blue dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact us</a>
               </li>
               <li>
-                <a href="DB_APIs/logout.php"
+                <a href="../DB_APIs/logout.php"
                   class="uppercase py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-custom-light-blue md:p-0 md:dark:hover:text-custom-light-blue dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Log out</a>
               </li>
             </ul>
