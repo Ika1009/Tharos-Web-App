c1a23b8d09ae497581af9497c60877d83243e395
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-23 00:47:49 +0200

    colors update for login and register page

--- login.php
+++ login.php
@@ -20,12 +20,12 @@
                 <div class="py-12 bg-indigo-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
                     <div class="cursor-pointer flex items-center">
                         <div><img class="w-12 h-12" src="images/ICON_1-768x767.png" alt="icon"></div>
-                        <div class="text-2xl text-indigo-800 tracking-wide ml-2 font-semibold">Tharros</div>
+                        <div class="text-2xl text-custom-dark-blue tracking-wide ml-2 font-semibold">Tharros</div>
                     </div>
                 </div>
                 <div class="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
-                    <h2 class="text-center text-4xl text-indigo-900 font-display font-semibold lg:text-left xl:text-5xl
-                    xl:text-bold">Log in</h2>
+                    <h2 class="text-center text-4xl text-custom-dark-blue font-display font-semibold lg:text-left xl:text-5xl xl:text-bold">
+                        Log in</h2>
                     <div class="mt-12">
                         <form>
                             <div>
@@ -37,19 +37,11 @@
                                     <div class="text-sm font-bold text-gray-700 tracking-wide">
                                         Password
                                     </div>
-                                    <div>
-                                        <a class="text-xs font-display font-semibold text-indigo-600 hover:text-indigo-800
-                                        cursor-pointer">
-                                            Forgot Password?
-                                        </a>
-                                    </div>
                                 </div>
                                 <input name="password" class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="password" id="password_input" placeholder="Enter your password">
                             </div>
                             <div class="mt-10">
-                                <button class="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
-                                font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
-                                shadow-lg" type="submit">
+                                <button class="bg-custom-dark-blue text-gray-100 p-4 w-full rounded-full tracking-wide font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-custom-dark-blue shadow-lg" type="submit">
                                     Log In
                                 </button>
                             </div>
