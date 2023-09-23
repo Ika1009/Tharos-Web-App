866be90c3411f09b1c218305353da3a506487dfa
Ika1009 <ilija.doncic@yahoo.com>
2023-09-23 15:26:41 +0200

    changed get_reports to use pdo

--- DB_APIs/get_reports.php
+++ DB_APIs/get_reports.php
@@ -12,30 +12,23 @@ if ($userID === null) {
     exit;
 }
 
-// Check if user ID is 0 (admin)
-if ($userID == 0) {
-    // If admin, get all reports
-    $stmt = $conn->prepare("SELECT * FROM reports");
-} else {
-    // If not admin, get reports only for that user
-    $stmt = $conn->prepare("SELECT * FROM reports WHERE user_id = ?");
-    $stmt->bind_param("i", $userID);
+// Prepare and execute the statement based on the user ID
+try {
+    if ($userID == 0) {
+        // If admin, get all reports
+        $stmt = $pdo->query("SELECT * FROM reports");
+    } else {
+        // If not admin, get reports only for that user
+        $stmt = $pdo->prepare("SELECT * FROM reports WHERE user_id = :userID");
+        $stmt->execute([':userID' => $userID]);
+    }
+
+    $reports = $stmt->fetchAll();
+
+    // Return the reports in JSON format
+    echo json_encode(['status' => 'success', 'reports' => $reports]);
+} catch (\PDOException $e) {
+    echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
 }
 
-// Execute the statement
-$stmt->execute();
-
-// Fetch the result
-$result = $stmt->get_result();
-$reports = [];
-while ($row = $result->fetch_assoc()) {
-    $reports[] = $row;
-}
-
-// Close the statement and connection
-$stmt->close();
-$conn->close();
-
-// Return the reports in JSON format
-echo json_encode(['status' => 'success', 'reports' => $reports]);
 ?>
