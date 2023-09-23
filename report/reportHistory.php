90e4d25788b87c8c96cf71f053308ba261537c09
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-23 20:23:26 +0200

    better responsive

--- report/reportHistory.php
+++ report/reportHistory.php
@@ -30,7 +30,7 @@
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" /></svg>
             </button>
         </div>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+        <div class="items-center justify-between hidden w-full custom:flex custom:w-auto custom:order-1" id="navbar-cta">
         <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-custom-dark-blue dark:bg-custom-dark-blue md:dark:bg-custom-dark-blue dark:border-custom-dark-blue">
             <li>
             <a href="https://tharros.net" class="uppercase py-2 pl-3 pr-4 text-white md:hover:text-custom-light-blue md:dark:hover:text-custom-light-blue rounded md:bg-transparent md:p-0">Home</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
           'custom-dark-blue': '#0e0d2f',
           'custom-light-blue': '#0bc1f5'
         },
+
+        screens: {
+          'custom': '830px',
+        },
       },
     },
     plugins: [],
