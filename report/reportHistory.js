802837751613d15f0fa6b338b263389411900616
Ika1009 <ilija.doncic@yahoo.com>
2023-09-23 15:08:48 +0200

    report writing out

--- report/reportHistory.js
+++ report/reportHistory.js
@@ -6,27 +6,43 @@ async function getReport(userId) {
             throw new Error('Network response was not ok');
         }
 
-        const data = await response.json();
+        const reports = await response.json();
 
-        console.log(data);
-
-        if (data.error) {
-            console.error(data.error);
+        if (!Array.isArray(reports) || !reports.length) {
+            console.error('No reports found');
             return;
         }
 
-        const answers = JSON.parse(data.answers);
-        const comments = JSON.parse(data.comments);
+        const reportContainer = document.querySelector('.grid'); // Container for the reports
 
-        // Assuming you have some elements to populate in your HTML
-        /*for (let i = 2; i <= 111; i++) {
-            let answerElement = document.getElementById('q' + i);  // Example elements
-            let commentElement = document.getElementById('comment' + i);
+        // Clear any existing report cards
+        reportContainer.innerHTML = '';
 
-            answerElement.value = answers[i - 2];
-            commentElement.value = comments[i - 2];
-        }*/
+        reports.forEach(report => {
+            const card = document.createElement('div');
+            card.classList.add('bg-white', 'rounded-lg', 'p-4', 'shadow');
+
+            const title = document.createElement('h3');
+            title.classList.add('text-xl', 'font-medium', 'mb-2');
+            title.textContent = report.facilityName || `Report Title ${report.id}`; 
+
+            const address = document.createElement('p');
+            address.classList.add('text-gray-700', 'mb-2');
+            address.textContent = report.address || `Report Description ${report.id}`;
+
+            const link = document.createElement('a');
+            link.href = `#`; // Add your report link here
+            link.classList.add('text-blue-600', 'hover:underline');
+            link.textContent = 'View Report';
+
+            card.appendChild(title);
+            card.appendChild(address);
+            card.appendChild(link);
+
+            reportContainer.appendChild(card);
+        });
+
     } catch (error) {
-        console.error('Error fetching the report:', error);
+        console.error('Error fetching the reports:', error);
     }
 }
