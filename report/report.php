b4a08473576d22a2d1e584f2674c4370f729b134
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-28 20:48:21 +0200

    semantic update

--- report/report.php
+++ report/report.php
@@ -144,14 +144,14 @@
                 <path d="M19.479 10.092c-.212-3.951-3.473-7.092-7.479-7.092-4.005 0-7.267 3.141-7.479 7.092-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.521-5.408zm-7.479-1.092l4 4h-3v4h-2v-4h-3l4-4z" />
               </svg>
             </i>
-            <p class="text-lg text-blue-700">Drop files to upload</p>
+            <p class="text-lg text-blue-700">Drop file to upload</p>
           </div>
 
           <!-- scroll area -->
           <section class="h-full overflow-auto p-8 w-full flex flex-col">
             <header class="border-dashed border-2 border-gray-400 py-12 flex flex-col justify-center items-center">
               <p class="mb-3 font-semibold text-gray-900 flex flex-wrap justify-center">
-                <span>Drag and drop your</span>&nbsp;<span>files anywhere or</span>
+                <span>Drag and drop your</span>&nbsp;<span>file anywhere or</span>
               </p>
               <input id="hidden-input" type="file" multiple class="hidden" />
               <button id="button" class="mt-2 rounded-sm px-3 py-1 bg-gray-200 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
@@ -166,7 +166,7 @@
             <ul id="gallery" class="flex flex-1 flex-wrap -m-1">
               <li id="empty" class="h-full w-full text-center flex flex-col justify-center items-center">
                 <img class="mx-auto w-32" src="https://user-images.githubusercontent.com/507615/54591670-ac0a0180-4a65-11e9-846c-e55ffce0fe7b.png" alt="no data" />
-                <span class="text-small text-gray-500">No files selected</span>
+                <span class="text-small text-gray-500">No file selected</span>
               </li>
             </ul>
           </section>
