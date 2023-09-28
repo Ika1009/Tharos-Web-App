35b9ea02517011435936997b15dc6dcbe3af6bc2
Ika1009 <ilija.doncic@yahoo.com>
2023-09-28 20:03:23 +0200

    now it should work for only one image

--- report/report.js
+++ report/report.js
@@ -502,14 +502,17 @@ function toggleMenu() {
 
 const fileTempl = document.getElementById("file-template"),
   imageTempl = document.getElementById("image-template"),
-  empty = document.getElementById("empty");
+  empty = document.getElementById("empty"),
+  gallery = document.getElementById("gallery"),
+  overlay = document.getElementById("overlay"),
+  hidden = document.getElementById("hidden-input");
 
-// use to store pre selected files
-let FILES = {};
+let FILE;
 
-// check if file is of type image and prepend the initialied
-// template to the target element
 function addFile(target, file) {
+  FILE = undefined;
+  target.innerHTML = ''; // Clear the previous file
+  
   const isImage = file.type.match("image.*"),
     objectURL = URL.createObjectURL(file);
 
@@ -520,99 +523,66 @@ function addFile(target, file) {
   clone.querySelector("h1").textContent = file.name;
   clone.querySelector("li").id = objectURL;
   clone.querySelector(".delete").dataset.target = objectURL;
-  clone.querySelector(".size").textContent =
-    file.size > 1024
-      ? file.size > 1048576
-        ? Math.round(file.size / 1048576) + "mb"
-        : Math.round(file.size / 1024) + "kb"
-      : file.size + "b";
+  clone.querySelector(".size").textContent = 
+    file.size > 1048576 
+      ? Math.round(file.size / 1048576) + "mb" 
+      : Math.round(file.size / 1024) + "kb";
 
-  isImage &&
-    Object.assign(clone.querySelector("img"), {
-      src: objectURL,
-      alt: file.name
-    });
+  isImage && Object.assign(clone.querySelector("img"), {
+    src: objectURL,
+    alt: file.name
+  });
 
   empty.classList.add("hidden");
   target.prepend(clone);
 
-  FILES[objectURL] = file;
+  FILE = file;
 }
 
-const gallery = document.getElementById("gallery"),
-  overlay = document.getElementById("overlay");
-
-// click the hidden input of type file if the visible button is clicked
-// and capture the selected files
-const hidden = document.getElementById("hidden-input");
 document.getElementById("button").onclick = () => hidden.click();
 hidden.onchange = (e) => {
-  for (const file of e.target.files) {
-    addFile(gallery, file);
-  }
+  const file = e.target.files[0];
+  if (file) addFile(gallery, file);
 };
 
-// use to check if a file is being dragged
-const hasFiles = ({ dataTransfer: { types = [] } }) =>
-  types.indexOf("Files") > -1;
-
-// use to drag dragenter and dragleave events.
-// this is to know if the outermost parent is dragged over
-// without issues due to drag events on its children
-let counter = 0;
-
-// reset counter and append file to gallery when file is dropped
 function dropHandler(ev) {
   ev.preventDefault();
-  for (const file of ev.dataTransfer.files) {
+  const file = ev.dataTransfer.files[0];
+  if (file) {
     addFile(gallery, file);
     overlay.classList.remove("draggedover");
-    counter = 0;
   }
 }
 
-// only react to actual files being dragged
 function dragEnterHandler(e) {
   e.preventDefault();
-  if (!hasFiles(e)) {
-    return;
-  }
-  ++counter && overlay.classList.add("draggedover");
+  overlay.classList.add("draggedover");
 }
 
 function dragLeaveHandler(e) {
-  1 > --counter && overlay.classList.remove("draggedover");
+  overlay.classList.remove("draggedover");
 }
 
 function dragOverHandler(e) {
-  if (hasFiles(e)) {
-    e.preventDefault();
-  }
+  e.preventDefault();
 }
 
-// event delegation to caputre delete events
-// fron the waste buckets in the file preview cards
 gallery.onclick = ({ target }) => {
   if (target.classList.contains("delete")) {
     const ou = target.dataset.target;
-    document.getElementById(ou).remove(ou);
-    gallery.children.length === 1 && empty.classList.remove("hidden");
-    delete FILES[ou];
+    document.getElementById(ou).remove();
+    FILE = undefined;
+    empty.classList.remove("hidden");
   }
 };
 
-// print all selected files
 document.getElementById("submit").onclick = () => {
-  alert(`Submitted Files:\n${JSON.stringify(FILES)}`);
-  console.log(FILES);
+  alert(`Submitted File:\n${FILE ? FILE.name : 'No file selected.'}`);
 };
 
-// clear entire selection
 document.getElementById("cancel").onclick = () => {
-  while (gallery.children.length > 0) {
-    gallery.lastChild.remove();
-  }
-  FILES = {};
+  gallery.innerHTML = ''; // Clear the gallery
+  FILE = undefined;
   empty.classList.remove("hidden");
   gallery.append(empty);
 };
