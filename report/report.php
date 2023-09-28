d99b1ad8cf12c9d47282becfc3c382707072992e
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-28 19:43:22 +0200

    color

--- report/report.php
+++ report/report.php
@@ -173,7 +173,7 @@
 
           <!-- sticky footer -->
           <footer class="flex justify-end px-8 pb-8 pt-4">
-            <button id="submit" class="rounded-sm px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
+            <button id="submit" class="rounded-sm px-3 py-1 bg-custom-dark-blue hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
               Upload now
             </button>
             <button id="cancel" class="ml-3 rounded-sm px-3 py-1 hover:bg-gray-300 focus:shadow-outline focus:outline-none">
