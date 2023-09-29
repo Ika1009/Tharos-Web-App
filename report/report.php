1f349c7facad6b7da74ad540ffd2688f436f8c49
Milos Mitrovic <milosmitrovic005@gmail.com>
2023-09-29 16:48:30 +0200

    .

--- report/report.php
+++ report/report.php
@@ -295,9 +295,10 @@
     <button onclick="tryJSPDF()">
       Try jsPDF
     </button>
-    
+
     <script src="https://unpkg.com/jspdf-invoice-template@1.4.0/dist/index.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf-lib/1.16.0/pdf-lib.min.js"></script>
     <script src="report.js"></script>
 </body>
 
